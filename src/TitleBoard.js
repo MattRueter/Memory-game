6c5503f0b00fab16459901c7193f48af46a8d70e
robert <matthewrueter51@gmail.com>
2022-06-06 16:39:27 -0500

    create title board skeleton.

--- src/TitleBoard.js
+++ src/TitleBoard.js
@@ -6,7 +6,12 @@ import React from 'react';
 function TitleBoard() {
   return (
     <div>
-      <h1>Hello from TitleBoard.</h1>
+      <div className='titleBar'>
+        <h1>Memory Game</h1>
+        <p>Click on each card. Avoid clicking on the same card more than once in a round.</p>
+        <div className='currentScore'>Current Score:</div>
+        <div className='bestScore'>Best Score:</div>
+      </div>
 
     </div>
   );
