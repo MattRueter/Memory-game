907198a9ddccaa7fbf2fa89e6ba6f81925f4568a
robert <matthewrueter51@gmail.com>
2022-06-06 17:39:52 -0500

    delete unused score variables.

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import TitleBoard from './TitleBoard';
 
-//let currentScore = 3;
-//let bestScore = 6; 
 
 class Card extends React.Component{
 	constructor(props){
 		super(props);
-		this.state ={ currentScore:0, bestScore:0};
+		this.state ={ currentScore:0, bestScore:0 };
 	}
 	render(){
 		return(
