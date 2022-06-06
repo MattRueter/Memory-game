be35b255687419182f09989482ecf11262a163a2
robert <matthewrueter51@gmail.com>
2022-06-06 17:33:38 -0500

    move score variables to initial state in Card component constructor.

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import TitleBoard from './TitleBoard';
 
-let currentScore = 3;
-let bestScore = 6; 
+//let currentScore = 3;
+//let bestScore = 6; 
 
 class Card extends React.Component{
 	constructor(props){
 		super(props);
+		this.state ={ currentScore:0, bestScore:0};
 	}
 	render(){
 		return(
 			<div>
-				<TitleBoard currentScore={currentScore} bestScore={bestScore} />
+				<TitleBoard currentScore={this.state.currentScore} bestScore={this.state.bestScore} />
 				<p>hello from Card</p>
 			</div>
 		)
