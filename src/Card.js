d2fa980443635da624ca59c81857e27f62f893eb
robert <matthewrueter51@gmail.com>
2022-06-07 11:13:01 -0500

    bind click event to each card with access to card's id.

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './Card.css';
 import TitleBoard from './TitleBoard';
 
-const imageLibrary = ['image A', 'imgage B', 'image C', 'image D', 'image E', 'image F', 'image G', 'image H'];
-const images = imageLibrary.map((image) =>
-	<div className='card' key={image.toString()}>{image}</div>
-);
 
 class Card extends React.Component{
 	constructor(props){
 		super(props);
 		this.state ={ currentScore:0, bestScore:0, clickedImages:[] };
+		this.handleClick=this.handleClick.bind(this);
 	}
+
+	handleClick(image){
+		console.log('clicked '+image)
+	}
+
 	render(){
+		const imageLibrary = ['image A', 'imgage B', 'image C', 'image D', 'image E', 'image F', 'image G', 'image H'];
+		const images= imageLibrary.map((image) =>
+		<div className='card' key={image.toString()} onClick={this.handleClick.bind(this,image)}>{image}</div>
+		);
+
 		return(
 			<div>
 				<TitleBoard currentScore={this.state.currentScore} bestScore={this.state.bestScore} />
