d382d665e33ca80eb405e8eae60ab15f7a88696c
robert <matthewrueter51@gmail.com>
2022-06-07 14:18:29 -0500

    randomly display cards after clicking them.

--- src/Card.js
+++ src/Card.js
@@ -3,23 +3,61 @@ import './Card.css';
 import TitleBoard from './TitleBoard';
 
 const imageLibrary = ['image A', 'imgage B', 'image C', 'image D', 'image E', 'image F', 'image G', 'image H'];
+let imagesDisplayedRandomly=[];
+let count=[];
+let stop = false;
+
+function displayCardsRandomly(){
+	while(stop===false){
+		const randomNumber=Math.floor(Math.random()*(imageLibrary.length));
+		if(imagesDisplayedRandomly.length===imageLibrary.length){
+		  stop =true;
+		  	console.log('stop.');
+		}else if(randomNumber>imageLibrary.length){
+			console.log('error. # too big.');
+		}else if(count.includes(randomNumber)){
+			console.log('# taken.');
+		}else{
+		  imagesDisplayedRandomly.push(imageLibrary[randomNumber]);
+		  count.push(randomNumber);
+		}	
+	}
+};
+
+function reset(){
+	imagesDisplayedRandomly=[];
+	count=[];
+	stop = false;
+  }
+
 
 class Card extends React.Component{
 	constructor(props){
 		super(props);
-		this.state ={ currentScore:0, bestScore:0, clickedImages:[] };
+		this.state ={ currentScore:0, bestScore:0, clickedImages:[], clicked:false};
 		this.handleClick=this.handleClick.bind(this);
 	}
 
 	handleClick(image){
-		console.log('clicked '+image)
-		console.log(clickedImages.includes(image));
+		//console.log('clicked '+image)
+		//console.log(clickedImages.includes(image));
+		reset();
+		displayCardsRandomly();
+		this.setState({clicked:true});
 	}
 
+	
 	render(){
-		const images= imageLibrary.map((image) =>
-		<div className='card' key={image.toString()} onClick={this.handleClick.bind(this,image)}>{image}</div>
-		);
+		let images;
+		if(imagesDisplayedRandomly.length===0){
+			images= imageLibrary.map((image) =>
+			<div className='card' key={image.toString()} onClick={this.handleClick.bind(this,image)}>{image}</div>
+			);
+		}else{
+			images = imagesDisplayedRandomly.map((image) =>
+			<div className='card' key={image.toString()} onClick={this.handleClick.bind(this,image)}>{image}</div>
+			);
+		}
 
 		return(
 			<div>
