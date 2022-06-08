8b5b090549050402eb0d6fcd39a22ad0d3fd3816
robert <matthewrueter51@gmail.com>
2022-06-08 18:02:30 -0500

    simplify render. images only maps to imagesDisplayedRandomly which is called at the start of the program and updated on each click of a card.

--- src/Card.js
+++ src/Card.js
@@ -4,25 +4,26 @@ import TitleBoard from './TitleBoard';
 
 
 const imageLibrary = ['one.png', 'two.png', 'three.png','four.png'];
-let imagesDisplayedRandomly=[];
-
+let imagesDisplayedRandomly = [];
+displayCardsRandomly(); 
 
 function displayCardsRandomly(){
 	imagesDisplayedRandomly=[];
 	let count=[];
 	let stop = false;
-
-	while(stop===false){
-		const randomNumber=Math.floor(Math.random()*(imageLibrary.length));
-		if(imagesDisplayedRandomly.length===imageLibrary.length){
-		  stop =true;
-		}else if(randomNumber>imageLibrary.length){
-		}else if(count.includes(randomNumber)){
-		}else{
-		  imagesDisplayedRandomly.push(imageLibrary[randomNumber]);
-		  count.push(randomNumber);
-		}	
-	}
+		
+		while(stop===false){
+			const randomNumber=Math.floor(Math.random()*(imageLibrary.length));
+				if(imagesDisplayedRandomly.length===imageLibrary.length){
+					stop =true;
+				}else if(randomNumber>imageLibrary.length){
+				}else if(count.includes(randomNumber)){
+				}else{
+					imagesDisplayedRandomly.push(imageLibrary[randomNumber]);
+					count.push(randomNumber);
+				}	
+		}
+	return imagesDisplayedRandomly;
 };
 
 
@@ -31,7 +32,6 @@ class Card extends React.Component{
 		super(props);
 		this.state ={ currentScore:0, bestScore:0, clickedImages:[]};
 		this.handleClick=this.handleClick.bind(this);
-		
 	}
 	
 	handleClick(image){
@@ -42,21 +42,20 @@ class Card extends React.Component{
 				return {currentScore: state.currentScore+1}
 			}); 
 		}
+
 		displayCardsRandomly();
 	}
 
 	render(){
-		let images;
-		if(imagesDisplayedRandomly.length===0){
-			images= imageLibrary.map((image) =>
-			<div className='card' style={{backgroundImage:`url(${image})`}} key={image.toString()} onClick={this.handleClick.bind(this,image)}></div>
+		let images = imagesDisplayedRandomly.map((image) =>
+			<div 
+				className = 'card' 
+				style = {{backgroundImage:`url(${image})`}} 
+				key = {image.toString()} 
+				onClick = {this.handleClick.bind(this,image)}>
+			</div>
 			);
-		}else{
-			images = imagesDisplayedRandomly.map((image) =>
-			<div className='card' style={{backgroundImage:`url(${image})`}} key={image.toString()} onClick={this.handleClick.bind(this,image)}></div>
-			);
-		}
-
+		
 		return(
 			<div>
 				<TitleBoard currentScore={this.state.currentScore} bestScore={this.state.bestScore} />
