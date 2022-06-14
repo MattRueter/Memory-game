7a0beecd1c24548aee665fa5743b75c0927a8ae9
robert <matthewrueter51@gmail.com>
2022-06-14 14:47:49 -0500

    initial commit of WinDisplay.

---
+++ src/WinDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './WinDisplay.css';
+
+class WinDisplay extends React.Component{
+
+	render(){
+		return(
+			<div className='winMessageContainer'>
+				<h1>Well done!</h1>
+			</div>
+		)
+	}
+}
+
+export default WinDisplay;
